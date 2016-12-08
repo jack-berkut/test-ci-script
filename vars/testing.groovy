5a5efb2dec77ce336e30103ff3e16b31e1fc9dc2
jack <jack@jack.local>
2016-12-09 00:35:08 +0900

    test

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -1,10 +1,17 @@
 #!/bin/env groovy
 
 def call(body) {
+	// evaluate the body block, and collect configuration into the object
 	def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
-	println config
+    stage ("Checkout Code") {
+    	checkout scm
+    }
+
+    stage ("Tesing") {
+    	echo "hoge"
+    }
 }
